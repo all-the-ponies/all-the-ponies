fa1ac312fdb553091ee75189fefc8e3a23e7965b
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-01-17 16:17:06 -0700

    Change how base.css is imported

--- src/pages/+Head.vue
+++ src/pages/+Head.vue
@@ -1,12 +1,9 @@
-<!-- <script setup lang="ts">
-import { useData } from 'vike-vue/useData';
-import { usePageContext } from 'vike-vue/usePageContext';
-
-const pageContext = usePageContext()
-</script> -->
+<script setup lang="ts">
+import '@/assets/css/base.css'
+</script>
 
 <template>
     <!-- <meta property="og:title" :content="pageContext.config.title" /> -->
-    <link rel="stylesheet" href="/src/assets/css/base.css">
+    <!-- <link rel="stylesheet" href="@/assets/css/base.css"> -->
     <meta property="og:type" content="website" />
 </template>
