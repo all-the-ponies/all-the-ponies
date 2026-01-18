3f44a1b4145806b14bdde2428f4eae713695cd82
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-01-17 17:53:55 -0700

    Update absoluteUrl.ts

--- src/scripts/absoluteUrl.ts
+++ src/scripts/absoluteUrl.ts
@@ -3,7 +3,8 @@ export default function absoluteUrl(path: string) {
     try {
         domain = location.origin
     } catch {
-        domain = process?.env?.VERCEL_URL || 'https://all-the-ponies.com'
+        console.log('vercel url', process?.env?.VERCEL_URL)
+        domain = 'https://all-the-ponies.com'
     }
     
     return String(new URL(path, domain))
