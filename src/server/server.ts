76f2a502c7b41d1928483d8ee02c970aa7383fb1
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-01-19 13:14:02 -0700

    Revert "switch to express"
    This reverts commit 900d790476a401d3973f450c56b5d739df7ba78f.

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,13 +1,13 @@
 // server/index.js
 
-import express from 'express'
-import { apply, serve } from '@photonjs/express'
+import { Hono } from 'hono'
+import { apply, serve } from '@photonjs/hono'
 
 function startServer() {
-    console.log('starting server')
-    const app = express()
-    apply(app)
-    return serve(app)
+  console.log('starting server')
+  const app = new Hono()
+  apply(app)
+  return serve(app)
 }
 
 export default startServer()
