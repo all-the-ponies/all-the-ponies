390bf8ee50c8bb28f8e2ffb344aba20f4c2e44ea
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-01-19 22:56:17 -0700

    Add more og tags

--- src/pages/+Head.vue
+++ src/pages/+Head.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
 import '@/assets/css/base.css'
-
 </script>
 
 <template>
-    <!-- <meta property="og:title" :content="pageContext.config.title" /> -->
-    <!-- <link rel="stylesheet" href="@/assets/css/base.css"> -->
-    <meta property="og:type" content="website" />
 </template>
--- src/pages/+Layout.vue
+++ src/pages/+Layout.vue
@@ -1,16 +1,36 @@
 <script setup lang="ts">
+import { language } from '@/globals';
+import { LOCALES } from '@/i18n';
+import absoluteUrl from '@/scripts/absoluteUrl';
 import SidebarView from '@/views/SidebarView.vue'
+import { Head } from 'vike-vue/Head';
+import { usePageContext } from 'vike-vue/usePageContext';
+
+const pageContext = usePageContext()
+
 </script>
 
 <template>
     <!-- <Config :title="$t('site.title')" :description="$t('site.description')"></Config> -->
+    <Head>
+        <meta property="og:locale" :content="language.code.replace('-', '_')" />
+        <meta
+            v-for="code in Object.keys(LOCALES).filter(code => code !== language.code)"
+            property="og:locale:alternate"
+            :content="code.replace('-', '_')"
+        />
+        
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" :content="$t('site.title')" />
+        <meta property="og:url" :content="absoluteUrl(pageContext.urlOriginal)" />
+    </Head>
 
-  <div id="main" class="page" ref="main">
-    <SidebarView />
-    <main class="router">
-        <slot></slot>
-    </main>
-  </div>
+    <div id="main" class="page" ref="main">
+        <SidebarView />
+        <main class="router">
+            <slot></slot>
+        </main>
+    </div>
 </template>
 
 <style scoped>
