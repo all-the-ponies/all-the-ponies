bedfddf67722660d06722b9b0c3062fdc29b5566
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-02-13 00:58:28 -0700

    Rewrite description to be shorter
    I realize nobody is reading that big paragraph, so it's better to keep it short, and prioritize the browse section more

--- src/pages/index/+Page.vue
+++ src/pages/index/+Page.vue
@@ -57,16 +57,13 @@ function openSidebar() {
     <div class="home-page">
         <section class="section">
             <h1 >{{ $t('site.title') }}</h1>
-            <p>
-                Welcome to All The Ponies, where you can see All The Ponies from the
-                <Link class="link" href="https://www.gameloft.com/game/my-little-pony">My Little Pony Gameloft</Link> mobile game, as well as many
-                other stuff. If you're a player of the game, this is the perfect website to
-                gather information, track what you have, and more. If you're not a player, then
-                it's still a great website. Since the game has over 2,000 ponies comprised of most
-                friendship is Magic characters as well as some G1 and G3 ponies, it can be fun to
-                scroll through them. You may also enjoy the <Link href="/quiz/" class="link">Pony Quiz</Link> to see
-                how many ponies you can name.
-            </p>
+            <i18n-t keypath="home.description" tag="p">
+                <template #game_link>
+                    <Link class="link" href="https://www.gameloft.com/game/my-little-pony">
+                        {{ $t('home.message.game_name') }}
+                    </Link>
+                </template>
+            </i18n-t>
         </section>
 
         <section class="section">
