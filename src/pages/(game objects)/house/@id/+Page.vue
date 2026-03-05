daa7f684257864b453b4a1310c53098aba42c50b
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-03-04 22:52:14 -0700

    Switch to using `<ObjectPage>` layout

--- src/pages/(game objects)/house/@id/+Page.vue
+++ src/pages/(game objects)/house/@id/+Page.vue
@@ -13,22 +13,23 @@ import { Config } from 'vike-vue/Config'
 import absoluteUrl from '@/scripts/absoluteUrl'
 import Link from '@/components/Link.vue'
 import { useData } from 'vike-vue/useData'
+import ObjectPage from '@/layouts/ObjectPage.vue';
 
 
 const pageContext = usePageContext()
 const data = useData<{house: HouseType}>()
 
-const objectInfo = computed(() => data.house)
+const house = computed(() => data.house)
 
 
 const name = computed(() => {
-    let name = gameData.translateName(objectInfo.value).value
+    let name = gameData.translateName(house.value).value
     return name
 })
 
 const residents = computed(() => {
     const residents: {[ L in Location ]+?: PonyType[]} = {}
-    for (let ponyId of objectInfo.value.residents) {
+    for (let ponyId of house.value.residents) {
         let pony = gameData.getObject(ponyId, 'pony')
         if (!(pony.location in residents)) {
             residents[pony.location] = []
@@ -42,126 +43,96 @@ const residents = computed(() => {
 </script>
 
 <template>
-    <Config :title="name" description="" :image="absoluteUrl(staticImage(objectInfo.image.main))"></Config>
+    <Config :title="name" description="" :image="absoluteUrl(staticImage(house.image.main))"></Config>
 
     <div>
         <back-button/>
-        <div v-if="objectInfo === null">
+        <div v-if="house === null">
             House {{ pageContext.routeParams.id }} not found
         </div>
-        <div v-else class="object-profile">
-            <div>
-                <h1 class="name">
-                    {{ name }}
-                </h1>
-                <img class="full-image" :src="staticImage(objectInfo.image.main)" :alt="name">
-            </div>
-            <div>
-                <table class="infobox">
-                    <tbody>
-                        <tr>
-                            <th colspan="2">{{ $t('common.info') }}</th>
-                        </tr>
-                        <tr>
-                            <td>{{ $t('location.town') }}</td>
-                            <td>
-                                {{
-                                    new Intl.ListFormat(language.code, {
-                                            style: 'short',
-                                        })
-                                        .format(
-                                            objectInfo.location
-                                            .map((location) => $t(LOCATIONS[location].string))
-                                        )
-                                }}
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>{{ $t('game_object.common.size') }}</td>
-                            <td>{{ objectInfo.grid_size }}x{{ objectInfo.grid_size }}</td>
-                        </tr>
-                        <tr>
-                            <td>{{ $t('game_object.building.build_time') }}</td>
-                            <td>{{ formatTime(objectInfo.build.time) }}</td>
-                        </tr>
-                        <tr>
-                            <td>{{ $t('game_object.building.build_skip_cost') }}</td>
-                            <td><currency-image object="Gems">{{ objectInfo.build.skip_cost }}</currency-image></td>
-                        </tr>
-                        <tr>
-                            <td>{{ $t('game_object.building.build_reward') }}</td>
-                            <td><currency-image object="XP">{{ objectInfo.build.xp }}</currency-image></td>
-                        </tr>
-                        <tr v-if="Object.keys(residents).length > 0">
-                            <th colspan="2">{{ $t('game_object.house.resident', 2) }}</th>
-                        </tr>
-                        <tr v-for="(ponies, location) in residents">
-                            <td colspan="2">
-                                <span v-if="objectInfo.location.length > 1">{{ $t(LOCATIONS[location].string) }} <br></br></span>
-                                <div class="residents">
-                                    <Link
-                                        v-for="pony in ponies"
-                                        :href="`/${pony.category}/${pony.id}`"
-                                        class="resident link"
-                                    >
-                                        <img :src="staticImage(pony.image.portrait)" :alt="pony.name[language.key]" :title="pony.name[language.key]">
-                                        <span class="resident-name">{{ gameData.translateName(pony) }}</span>
-                                    </Link>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr v-if="objectInfo.visitors.length > 0">
-                            <th colspan="2">{{ $t('game_object.house.visitor', 2) }}</th>
-                        </tr>
-                        <tr>
-                            <td colspan="2">
-                                <div class="residents">
-                                    <Link
-                                        v-for="pony in objectInfo.visitors.map((id) => gameData.getObject(id, 'pony'))"
-                                        :href="`/${pony.category}/${pony.id}`"
-                                        class="resident link"
-                                     >
-                                        <img :src="staticImage(pony.image.portrait)" :alt="pony.name[language.key]" :title="pony.name[language.key]">
-                                        <span class="resident-name">{{ gameData.translateName(pony) }}</span>
-                                    </Link>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+        <template v-else>
+            <ObjectPage :game-object="house">
+                <template #info>
+                    <table class="infobox">
+                        <tbody>
+                            <tr>
+                                <th colspan="2">{{ $t('common.info') }}</th>
+                            </tr>
+                            <tr>
+                                <td>{{ $t('location.town') }}</td>
+                                <td>
+                                    {{
+                                        new Intl.ListFormat(language.code, {
+                                                style: 'short',
+                                            })
+                                            .format(
+                                                house.location
+                                                .map((location) => $t(LOCATIONS[location].string))
+                                            )
+                                    }}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>{{ $t('game_object.common.size') }}</td>
+                                <td>{{ house.grid_size }}x{{ house.grid_size }}</td>
+                            </tr>
+                            <tr>
+                                <td>{{ $t('game_object.building.build_time') }}</td>
+                                <td>{{ formatTime(house.build.time) }}</td>
+                            </tr>
+                            <tr>
+                                <td>{{ $t('game_object.building.build_skip_cost') }}</td>
+                                <td><currency-image object="Gems">{{ house.build.skip_cost }}</currency-image></td>
+                            </tr>
+                            <tr>
+                                <td>{{ $t('game_object.building.build_reward') }}</td>
+                                <td><currency-image object="XP">{{ house.build.xp }}</currency-image></td>
+                            </tr>
+                            <tr v-if="Object.keys(residents).length > 0">
+                                <th colspan="2">{{ $t('game_object.house.resident', 2) }}</th>
+                            </tr>
+                            <tr v-for="(ponies, location) in residents">
+                                <td colspan="2">
+                                    <span v-if="house.location.length > 1">{{ $t(LOCATIONS[location].string) }} <br></br></span>
+                                    <div class="residents">
+                                        <Link
+                                            v-for="pony in ponies"
+                                            :href="`/${pony.category}/${pony.id}`"
+                                            class="resident link"
+                                        >
+                                            <img :src="staticImage(pony.image.portrait)" :alt="pony.name[language.key]" :title="pony.name[language.key]">
+                                            <span class="resident-name">{{ gameData.translateName(pony) }}</span>
+                                        </Link>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr v-if="house.visitors.length > 0">
+                                <th colspan="2">{{ $t('game_object.house.visitor', 2) }}</th>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <div class="residents">
+                                        <Link
+                                            v-for="pony in house.visitors.map((id) => gameData.getObject(id, 'pony'))"
+                                            :href="`/${pony.category}/${pony.id}`"
+                                            class="resident link"
+                                         >
+                                            <img :src="staticImage(pony.image.portrait)" :alt="pony.name[language.key]" :title="pony.name[language.key]">
+                                            <span class="resident-name">{{ gameData.translateName(pony) }}</span>
+                                        </Link>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </template>
+            </ObjectPage>
+            
+        </template>
     </div>
 </template>
 
 <style lang="css" scoped>
-
-.object-profile {
-    display: grid;
-    grid-template-columns: 50% 50%;
-}
-
-@media screen and (max-width: 40rem) {
-    .object-profile {
-        grid-template-columns: auto;
-    }
-}
-
-.name {
-    display: flex;
-    align-items: center;
-    gap: 0.2em;
-}
-
-.portrait {
-    height: 1em;
-}
-
-.full-image {
-    max-height: 10rem;
-    object-fit: contain;
-    object-position: center;
-}
 
 .residents {
     --item-width: 5rem;
@@ -181,12 +152,12 @@ const residents = computed(() => {
     width: 100%;
     justify-items: center;
     align-items: stretch;
-}
-
-.resident img {
-    object-fit: contain;
-    object-position: center;
-    width: var(--item-width);
+    
+    img {
+        object-fit: contain;
+        object-position: center;
+        width: var(--item-width);
+    }
 }
 
 </style>
