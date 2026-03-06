9d0a85f092deaf25e2df890e5518453051fdfa45
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-03-05 23:25:47 -0700

    Start working on wishlists

---
+++ src/components/lists/createList/CreateListDialog.vue
@@ -0,0 +1,95 @@
+<script setup lang="ts">
+import DialogComponent from '@/components/DialogComponent.vue';
+import SelectObjectDialog from '@/components/dialogs/SelectObjectDialog.vue';
+import GameCard from '@/components/GameCard.vue';
+import { staticImage } from '@/scripts/common';
+import gameData from '@/scripts/gameData';
+import { useListStore } from '@/stores/listStore';
+import type { GameObjectId } from '@/types/gameDataTypes';
+import { computed, ref, useTemplateRef } from 'vue';
+
+const listsStore = useListStore()
+
+const createListDialog = useTemplateRef('create-list')
+const selectItemDialog = useTemplateRef('select-object-dialog')
+
+const name = ref<string>()
+const image = ref<{item: GameObjectId | null, image: string | null}>()
+const gameObject = computed(() => gameData.getObject(image.value?.item))
+
+
+function createList() {
+    name.value = ''
+    image.value = {
+        item: 'Pony_Twilight_Sparkle',
+        image: 'main',
+    }
+    createListDialog.value.open()
+}
+
+function submitList() {
+    listsStore.createList(
+        name.value,
+        image.value,
+        new Date(),
+    )
+}
+
+function cancelList() {
+
+}
+
+function selectImage() {
+    selectItemDialog.value.open()
+}
+
+function selectedImage() {
+
+}
+
+defineExpose({
+    createList
+})
+
+</script>
+
+<template>
+    <DialogComponent
+        ref="create-list"
+        :title="$t('lists.dialog.create_list.title')"
+        @submit="submitList"
+        @cancel="cancelList"
+    >
+
+        {{ $t('lists.dialog.create_list.message.name') }}
+        <input
+            v-model="name"
+            class="text-box"
+            :placeholder="$t('lists.dialog.create_list.message.wishlist')"
+            type="text"
+            name="list-name"
+        >
+
+        <GameCard
+            :title="name || $t('lists.dialog.create_list.message.wishlist')"
+            hover
+            :image="staticImage(gameObject ? gameObject.image[image.image] : null)"
+            @click="selectImage()"
+        ></GameCard>
+
+        <template #menu>
+            <button @click="createListDialog.submit()" class="button button-green">{{ $t('button.ok') }}</button>
+            <button @click="createListDialog.cancel()" class="button button-red">{{ $t('button.cancel') }}</button>
+        </template>
+    </DialogComponent>
+
+    <SelectObjectDialog
+        ref="select-object-dialog"
+    >
+
+    </SelectObjectDialog>
+</template>
+
+<style lang="css" scoped>
+
+</style>
---
+++ src/pages/(lists)/lists/+Page.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import CreateListDialog from '@/components/lists/createList/CreateListDialog.vue';
+import { useListStore } from '@/stores/listStore';
+import { useTemplateRef } from 'vue';
+
+const listsStore = useListStore()
+const createListDialog = useTemplateRef('create-list-dialog')
+
+console.log(listsStore.lists)
+
+function createList() {
+    createListDialog.value.createList()
+}
+
+</script>
+
+<template>
+    <section>
+        <h1>{{ $t('lists.title') }}</h1>
+        <button @click="createList" class="button button-blue">{{ $t('lists.create_list') }}</button>
+    </section>
+
+    <CreateListDialog ref="create-list-dialog"></CreateListDialog>
+</template>
