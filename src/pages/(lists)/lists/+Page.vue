74464285a66591757f8a129590966a638543d7ed
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-03-07 14:44:03 -0700

    Show lists

--- src/pages/(lists)/lists/+Page.vue
+++ src/pages/(lists)/lists/+Page.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
+import GameCard from '@/components/GameCard.vue';
 import CreateListDialog from '@/components/lists/createList/CreateListDialog.vue';
-import { useListStore } from '@/stores/listStore';
+import SearchComponent from '@/components/SearchComponent.vue';
+import { staticImage, transformName } from '@/scripts/common';
+import gameData from '@/scripts/gameData';
+import { useListStore, type Wishlist } from '@/stores/listStore';
 import { useTemplateRef } from 'vue';
 
 const listsStore = useListStore()
@@ -12,13 +16,52 @@ function createList() {
     createListDialog.value.createList()
 }
 
+function searchFilters(query: string, items: Wishlist[]) {
+    const normalizedQuery = transformName(query)
+    
+    return items.filter(item => 
+        transformName(item.name).includes(normalizedQuery)
+    )
+}
+
+function getListImage(wishlist: Wishlist) {
+    const gameObject = gameData.getObject(wishlist.image.item)
+    if (!gameObject) {
+        return
+    }
+    return gameObject.image[wishlist.image.image]
+}
+
 </script>
 
 <template>
-    <section>
-        <h1>{{ $t('lists.title') }}</h1>
-        <button @click="createList" class="button button-blue">{{ $t('lists.create_list') }}</button>
-    </section>
+    <div>
+        <section class="section">
+            <h1>{{ $t('lists.title') }}</h1>
+            <button @click="createList" class="button button-blue">{{ $t('lists.create_list') }}</button>
+        </section>
+        <SearchComponent
+            :data="[...listsStore.lists.values()]"
+            :search-function="searchFilters"
+            :placeholder="$t('Wishlist')"
+        >
+            <template #item="{ item }">
+                <GameCard
+                    :title="item.name"
+                    :image="staticImage(getListImage(item))"
+                >
+                    <template #right>
+                        <button
+                            class="button-circle button-red"
+                            @click.stop.prevent="listsStore.lists.delete(item.id)"
+                        >
+                            -
+                        </button>
+                    </template>
+                </GameCard>
+            </template>
+        </SearchComponent>
+    </div>
 
     <CreateListDialog ref="create-list-dialog"></CreateListDialog>
 </template>
