31d094464820a7aa880e4a8d6d95a7e7848e0017
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-04-28 23:30:22 -0600

    Credit @justfeydan

--- src/pages/about/+Page.vue
+++ src/pages/about/+Page.vue
@@ -32,6 +32,7 @@ import { language } from '@/globals';
             <ul class="list">
                 <li><span class="text-green">Pamcezya</span> - {{ new Intl.DisplayNames([language.code], { type: "language" }).of('tr') }}</li>
                 <li><span class="text-green">Double Dove</span> - {{ new Intl.DisplayNames([language.code], { type: "language" }).of('zh') }}</li>
+                <li><span class="text-green">justfeydan</span> - {{ new Intl.DisplayNames([language.code], { type: "language" }).of('ru') }}</li>
             </ul>
         </section>
     </div>
