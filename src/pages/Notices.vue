a5159085a3c654e29e20981b1528098edbbe6a3a
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-05-02 04:24:26 -0600

    Add notice for corrupt save

--- src/pages/Notices.vue
+++ src/pages/Notices.vue
@@ -1,20 +1,34 @@
 <script setup lang="ts">
 import DiscordButton from '@/components/buttons/socials/DiscordButton.vue';
+import Link from '@/components/Link.vue';
 import Notice from '@/components/notice/Notice.vue'
 import links from '@/globals/links';
 
 </script>
 
 <template>
+    <Notice noticeId="corrupt-save">
+        <div class="notice-container">
+            <div class="main-body">
+                <div class="body">
+                    <h2>{{ $t('notices.corrupt_save.title') }}</h2>
+                    <p>{{ $t('notices.corrupt_save.body') }}</p>
+                </div>
+                <div>
+                    <Link class="notice-button button button-blue" href="/check-save/">{{ $t('notices.corrupt_save.button') }}</Link>
+                </div>
+            </div>
+        </div>
+    </Notice>
     <Notice noticeId="help-wanted">
-        <div class="help-wanted-container">
-            <div class="help-wanted">
+        <div class="notice-container">
+            <div class="main-body">
                 <div class="body">
                     <h2>{{ $t('notices.help_wanted.title') }}</h2>
                     <p>{{ $t('notices.help_wanted.body') }}</p>
                 </div>
                 <div>
-                    <DiscordButton class="discord-button" :href="links.discordServer">{{ $t('notices.help_wanted.discord') }}</DiscordButton>
+                    <DiscordButton class="notice-button" :href="links.discordServer">{{ $t('notices.help_wanted.discord') }}</DiscordButton>
                 </div>
             </div>
         </div>
@@ -22,26 +36,30 @@ import links from '@/globals/links';
 </template>
 
 <style lang="css" scoped>
-.help-wanted-container {
+.notice-container {
     container-type: inline-size;
 
-    .help-wanted {
+    .main-body {
         display: flex;
         align-items: center;
+        gap: 0.5rem;
     
         .body {
             min-width: 3rem;
         }
     
-        .discord-button {
+        .notice-button {
+            margin-top: 0;
             width: max-content;
             margin-inline: auto;
+            white-space: nowrap;
         }
     }
     
     @container (width < 30rem) {
-      .help-wanted {
+      .main-body {
         flex-wrap: wrap;
+        gap: 1rem;
       }
     }
 }
