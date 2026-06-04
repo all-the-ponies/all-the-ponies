fd3b5aba58237edcfb2a8094a51ad2d2859ddb0f
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-06-04 15:15:05 -0600

    Fetch translators from weblate

--- src/pages/about/+data.server.ts
+++ src/pages/about/+data.server.ts
@@ -2,20 +2,122 @@ import type { PageContextServer } from "vike/types"
 import { env } from 'cloudflare:workers'
 import ky from "ky"
 
-export type Data = {}
+export type Data = {
+    credits: Record<string, Translator>,
+    error?: string,
+}
 
-export async function data(pageContext: PageContextServer) {
-    const url = new URL('https://hosted.weblate.org/api/components/all-the-ponies/strings/credits/')
+interface WeblateUser {
+    email: string,
+    username: string,
+    full_name: string,
+    change_count: string,
+    date_joined: string,
+}
 
-    url.searchParams.set('start', '2026-06-01T00:00:00.000Z')
-    url.searchParams.set('end', new Date(Number(__BUILD_DATE__)).toISOString())
+export interface Translator {
+    username: string,
+    full_name: string,
+    source: 'weblate' | 'github',
+    languages: string[],
+}
 
-    const credits = await ky(url, {
-        headers: {
-            'Accept': 'application/json',
-            'Authorization': `Token ${env.WEBLATE_API_TOKEN}`,
+export async function data(pageContext: PageContextServer): Data {
+    try {
+        const languages = await ky<Record<'name' | 'code', string>[]>('https://hosted.weblate.org/api/projects/all-the-ponies/languages/', {
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': `Token ${env.WEBLATE_API_TOKEN}`,
+            }
+        }).json()
+    
+        const languageMap = {}
+    
+        console.log('languages', languages)
+    
+        for (let langData of languages) {
+            languageMap[langData.name] = langData.code
         }
-    }).json()
-
-    return { credits }
+    
+        console.log('languageMap', languageMap)
+    
+        
+        const url = new URL('https://hosted.weblate.org/api/components/all-the-ponies/strings/credits/')
+    
+        url.searchParams.set('start', '2026-06-01T00:00:00.000Z')
+        url.searchParams.set('end', new Date(Number(__BUILD_DATE__)).toISOString())
+    
+        const rawCredits = await ky<Record<string, WeblateUser[]>[]>(url, {
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': `Token ${env.WEBLATE_API_TOKEN}`,
+            }
+        }).json()
+    
+        console.log('rawCredits', rawCredits)
+    
+        const credits: Record<string, Translator> = {}
+    
+        for (let creditData of rawCredits) {
+            for (let [language, users] of Object.entries(creditData)) {
+                let code = languageMap[language]
+                for (let user of users) {
+                    // Remove myself from translation credits
+                    if (user.username === 'ego-lay-atman-bay') {
+                        continue
+                    }
+    
+                    if (user.username in credits) {
+                        if (!credits[user.username].languages.includes(code)) {
+                            credits[user.username].languages.push(code)
+                        }
+                    } else {
+                        credits[user.username] = {
+                            username: user.username,
+                            full_name: user.full_name,
+                            source: 'weblate',
+                            languages: [code],
+                        }
+                    }
+                }
+            }
+        }
+    
+        const extras: Translator[] = [
+            {
+                username: 'Pamcezya',
+                full_name: 'Pamcezya',
+                source: 'github',
+                languages: ['tr'],
+            },
+            {
+                username: 'double_dove',
+                full_name: 'Double Dove',
+                source: 'github',
+                languages: ['zh'],
+            },
+            {
+                username: 'justfeydan',
+                full_name: 'feydan',
+                source: 'github',
+                languages: ['ru'],
+            },
+            {
+                username: 'Hurmeow',
+                full_name: 'Hurmeow',
+                source: 'github',
+                languages: ['ru'],
+            },
+        ]
+    
+        for (let user of extras) {
+            if (!(user.username in credits)) {
+                credits[user.username] = user
+            }
+        }
+    
+        return { credits } satisfies Data
+    } catch (error) {
+        return {error: String(error), credits: null}
+    }
 }
