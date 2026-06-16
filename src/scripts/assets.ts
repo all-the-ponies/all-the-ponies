089e983222ca8e35965860476bea9d8b22d64ab4
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-06-15 19:43:42 -0600

    Implement assets proxy

--- src/scripts/assets.ts
+++ src/scripts/assets.ts
@@ -1,10 +1,27 @@
-const ASSETS_URL = 'https://assets.all-the-ponies.com'
+import { isDev } from "./common"
+
+const ASSETS_URL = __BASE_URL__
 
 export function createAssetUrl(asset: string) {
     if (!asset) {
         return null
     }
+
+    let baseUrl: string
+    if (typeof location !== 'undefined') {
+        baseUrl = location.origin
+    } else if (isDev()) {
+        baseUrl = 'http://localhost:5000'
+    } else {
+        baseUrl = __BASE_URL__
+    }
+
+    if (!baseUrl.endsWith('/')) {
+        baseUrl += '/'
+    }
     
-    const url = new URL(asset, ASSETS_URL)
+    baseUrl = `${baseUrl}game-assets/`
+
+    const url = new URL(asset, baseUrl)
     return String(url)
 }
