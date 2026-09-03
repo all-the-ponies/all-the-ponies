d13385b5ee27aacb39a708ed15a839a6aae1bc47
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-09-02 23:35:37 -0600

    Allow `CurrencyImage` to be link

---
+++ src/components/ConditionalWrap.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { Component } from 'vue'
+
+defineProps<{
+  condition: boolean
+  wrapper: string | Component
+  wrapperProps?: Record<string, unknown>
+}>()
+</script>
+
+<template>
+  <component :is="wrapper" v-bind="wrapperProps" v-if="condition">
+    <slot />
+  </component>
+  <slot v-else />
+</template>
--- src/components/CurrencyImage.vue
+++ src/components/CurrencyImage.vue
@@ -1,15 +1,40 @@
 <script setup lang="ts">
-import type { GameObject } from '@/types/gameDataTypes.ts';
+import type { CategoryName, GameObject } from '@/types/gameDataTypes.ts';
 import ObjectImage from './ObjectImage.vue';
+import { computed } from 'vue';
+import { getObject } from '@/scripts/gameData.ts';
+import Link from './Link.vue';
+import ConditionalWrap from './ConditionalWrap.vue';
 
 const props = defineProps<{
     object: string | GameObject,
+    isLink?: boolean | CategoryName | CategoryName[],
 }>()
+
+const gameObject = computed(() => getObject(props.object))
+const link = computed(() => {
+    if (!props.isLink || !gameObject.value) {
+        return
+    }
+
+    let categories: CategoryName[] = ['token']
+    if (typeof props.isLink === 'string') {
+        categories = [props.isLink]
+    } else if (Array.isArray(props.isLink)) {
+        categories = [...props.isLink]
+    }
+
+    if (categories.includes(gameObject.value.category)) {
+        return `/${gameObject.value.category}/${gameObject.value.id}/`
+    }
+})
 
 </script>
 
 <template>
-    <slot></slot>&nbsp;<object-image class="object-image" :object="props.object"></object-image>
+    <slot></slot>&nbsp;<ConditionalWrap :condition="Boolean(link)" :wrapper="Link" :wrapper-props="{
+        href: link,
+    }"><object-image class="object-image" :object="props.object"></object-image></ConditionalWrap>
 </template>
 
 <style lang="css" scoped>
